659776b83b7c3f2bdabbf11c212a775740e10fb5
Ferran <ferran.docencia@gmail.com>
2024-04-29 10:47:01 +0200

    4 - Prueba POST

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
 
+
 // Ocultar formulario de alta 
 document.getElementById("signup").style.display = "none";
 
+// Funciones para mostrar y ocultar formularios
 function mostrarFormularioAlta() {
     document.getElementById("signin").style.display = "none";
     document.getElementById("signup").style.display = "block";
---
+++ php/signup.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once ('connection.php');
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
